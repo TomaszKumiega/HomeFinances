29733347390a42ff51f97621b793cff5bbaf012f
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-05-14 22:55:54 +0200

    Add CategoryType enum

--- HomeFinances.Model/Model/Category.cs
+++ HomeFinances.Model/Model/Category.cs
@@ -5,11 +5,18 @@ using System.Text;
 
 namespace HomeFinances.Model
 { 
+    public enum CategoryType
+    {
+        IncomeCategory,
+        ExpenseCategory
+    }
+
     public class Category
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public Color Color { get; set; }
+        public CategoryType Type { get; set; }
     }
 }
