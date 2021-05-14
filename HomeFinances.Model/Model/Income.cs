a7cc455c3a42df8b96f9ba2b50d7e9778df96b74
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-05-14 22:59:32 +0200

    Add Income model

---
+++ HomeFinances.Model/Model/Income.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeFinances.Model.Model
+{
+    public class Income : Transaction
+    {
+        private float _value;
+        private Category _category;
+
+        public override float Value
+        {
+            get => _value;
+            set
+            {
+                if (value < 0) throw new ArgumentException("Value cannot be negative");
+                _value = value;
+            }
+        }
+
+        public override Category Category
+        {
+            get => _category;
+            set
+            {
+                if (value.Type != CategoryType.IncomeCategory) throw new ArgumentException("Category type must be IncomCategory");
+                _category = value;
+            }
+        }
+    }
+}
