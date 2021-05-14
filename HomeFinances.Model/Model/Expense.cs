940e748ad14a0d8d63e81c44fafd79ea641da15e
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-05-14 23:02:52 +0200

    Add Expense model

---
+++ HomeFinances.Model/Model/Expense.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeFinances.Model.Model
+{
+    public class Expense : Transaction
+    {
+        private float _value;
+        private Category _category;
+
+        public override float Value
+        {
+            get => _value;
+            set
+            {
+                if (value >= 0) throw new ArgumentException("Value cannot be positive or 0");
+                _value = value;
+            }
+        }
+
+        public override Category Category
+        {
+            get => _category;
+            set
+            {
+                if (value.Type != CategoryType.ExpenseCategory) throw new ArgumentException("Category type must be ExpenseCategory");
+                _category = value;
+            }
+        }
+    }
+}
