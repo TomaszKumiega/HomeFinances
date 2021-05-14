e9ebe5b0f66748d98493cc563b6954366358e1dd
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-05-14 22:34:50 +0200

    Add Transaction model

---
+++ HomeFinances.Model/Model/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeFinances.Model
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Category Category { get; set; }
+        public DateTime Date { get; set; }
+        public float Value { get; set; }
+    }
+}
