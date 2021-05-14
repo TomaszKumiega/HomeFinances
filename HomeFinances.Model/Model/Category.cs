953f039c01ec661eaa2d80bd8959cd389a548902
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-05-14 22:34:34 +0200

    Add Category model

---
+++ HomeFinances.Model/Model/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace HomeFinances.Model
+{ 
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Color Color { get; set; }
+    }
+}
