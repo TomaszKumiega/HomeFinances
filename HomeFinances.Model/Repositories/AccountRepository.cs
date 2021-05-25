0c783d9294dd259a4f483bf144b59b7c7c89bfaf
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-05-26 01:18:21 +0200

    Add AccountRepository

---
+++ HomeFinances.Model/Repositories/AccountRepository.cs
@@ -0,0 +1,15 @@
+﻿using HomeFinances.Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeFinances.Model.Repositories
+{
+    public class AccountRepository : DapperRepository<Account>
+    {
+        public AccountRepository() : base("Accounts")
+        {
+
+        }
+    }
+}
