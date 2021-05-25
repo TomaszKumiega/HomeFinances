1daf820d5e88f601c020deea760286561ba6cd09
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-05-26 00:54:19 +0200

    Add Account model

---
+++ HomeFinances.Model/Model/Account.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeFinances.Model.Model
+{
+    public class Account
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+    }
+}
