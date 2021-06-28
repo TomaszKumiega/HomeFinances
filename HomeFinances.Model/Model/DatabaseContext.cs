d1e857f1f1ae74c1f3d44a4301a5eb9c74286d4a
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-28 12:24:25 +0200

    Add database context

---
+++ HomeFinances.Model/Model/DatabaseContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HomeFinances.Model.Model
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=HomeFinances.db; Version = 3");
+        }
+
+        public List<Account> Accounts { get; set; }
+        public List<Expense> Expenses { get; set; }
+        public List<Income> Incomes { get; set; }
+
+
+    }
+}
