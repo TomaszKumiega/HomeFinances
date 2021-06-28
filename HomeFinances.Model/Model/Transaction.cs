c530233f52d5dd0091e8cf90b916226c6265a4da
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-28 12:31:49 +0200

    Add account id property

--- HomeFinances.Model/Model/Transaction.cs
+++ HomeFinances.Model/Model/Transaction.cs
@@ -7,6 +7,7 @@ namespace HomeFinances.Model.Model
     public abstract class Transaction : IDatabaseEntity
     {
         public virtual Guid Id { get; set; }
+        public virtual Guid AccountId { get; set; }
         public virtual string Name { get; set; }
         public virtual Category Category { get; set; }
         public virtual DateTime Date { get; set; }
