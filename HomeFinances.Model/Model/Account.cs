5368166de1099af2a89ddb364d1f7661e2b82be0
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-29 01:55:48 +0200

    Add constructors

--- HomeFinances.Model/Model/Account.cs
+++ HomeFinances.Model/Model/Account.cs
@@ -6,6 +6,20 @@ namespace HomeFinances.Model.Model
 {
     public class Account
     {
+        public Account()
+        {
+
+        }
+
+        public Account(Guid id, string currency, string name, double balance)
+        {
+            Id = id;
+            Currency = currency;
+            Name = name;
+            Balance = balance;
+            Transactions = new List<Transaction>();
+        }
+
         public Guid Id { get; set; }
         public string Currency { get; set; }
         public string Name { get; set; }
