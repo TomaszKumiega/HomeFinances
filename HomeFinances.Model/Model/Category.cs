8358f9dc8bc486ed96ec8e81a6eb33e2a8713cdd
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-28 14:49:51 +0200

    Add ArgbColor property
    ArgbColor property will be used for database mapping of Color

--- HomeFinances.Model/Model/Category.cs
+++ HomeFinances.Model/Model/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Drawing;
 using System.Text;
 
@@ -16,7 +17,13 @@ namespace HomeFinances.Model.Model
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        [NotMapped]
         public Color Color { get; set; }
+        public Int32 ArgbColor
+        {
+            get => Color.ToArgb();
+            set => Color = Color.FromArgb(value);
+        }
         public CategoryType Type { get; set; }
     }
 }
