779bd139b712a1eb70c85a6e1ca4d06a4d6d2805
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-29 01:56:00 +0200

    Add AddAccountViewModel

---
+++ HomeFinances.ViewModel/ViewModels/AddAccountViewModel.cs
@@ -0,0 +1,28 @@
+﻿using HomeFinances.Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeFinances.ViewModel.ViewModels
+{
+    public class AddAccountViewModel
+    {
+        private DatabaseContext Context { get; }
+
+        public string Currency { get; set; }
+        public string Name { get; set; }
+        public double Balance { get; set; }
+
+        public AddAccountViewModel(DatabaseContext context)
+        {
+            Context = context;
+        }
+
+        public void AddAccount()
+        {
+            var account = new Account(Guid.NewGuid(), Currency, Name, Balance);
+            Context.Accounts.Add(account);
+            Context.SaveChanges();
+        }
+    }
+}
