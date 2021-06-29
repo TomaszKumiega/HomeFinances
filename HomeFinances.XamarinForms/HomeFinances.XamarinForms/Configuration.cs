6dac204067b9eeb8adcde2a4e496dbb36941d8ac
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-29 23:20:50 +0200

    Create configuration file

---
+++ HomeFinances.XamarinForms/HomeFinances.XamarinForms/Configuration.cs
@@ -0,0 +1,26 @@
+﻿using HomeFinances.Model.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HomeFinances.XamarinForms
+{
+    public class Configuration : IConfiguration
+    {
+        public string DatabaseFilePath 
+        {
+            get
+            {
+                var folderPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                var databasePath = Path.Combine(folderPath, this.DatabaseFileName);
+                return databasePath;
+            } 
+        }
+
+        public string DatabaseFileName
+        {
+            get => "HomeFinances.db";
+        }
+    }
+}
