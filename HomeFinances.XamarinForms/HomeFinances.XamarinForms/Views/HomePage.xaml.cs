a11ad9f950264b9749da8f42b7086c203b92aee6
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-29 19:35:59 +0200

    Add HomePage

---
+++ HomeFinances.XamarinForms/HomeFinances.XamarinForms/Views/HomePage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeFinances.ViewModel;
+using HomeFinances.ViewModel.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HomeFinances.XamarinForms.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        private IHomeViewModel ViewModel { get; }
+
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
