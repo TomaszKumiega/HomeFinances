f9511df99406e800b19d1fe3f23d342b59bf35a0
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-29 20:34:27 +0200

    Add dependency injection container config

---
+++ HomeFinances.XamarinForms/HomeFinances.XamarinForms/ContainerConfig.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using HomeFinances.Model.Model;
+using HomeFinances.ViewModel.Commands;
+using HomeFinances.ViewModel.ViewModels;
+using HomeFinances.XamarinForms.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeFinances.XamarinForms
+{
+    public static class ContainerConfig
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<DatabaseContext>().As<IDatabaseContext>();
+            builder.RegisterType<CommandFactory>().As<ICommandFactory>();
+            builder.RegisterType<HomeViewModel>().As<IHomeViewModel>();
+            builder.RegisterType<AddAccountViewModel>().As<IAddAccountViewModel>();
+            builder.RegisterType<AddTransactionViewModel>().As<IAddTransactionViewModel>();
+            builder.RegisterType<RecordsViewModel>().As<IRecordsViewModel>();
+            builder.RegisterType<AppShell>().AsSelf();
+            builder.RegisterType<HomePage>().AsSelf();
+
+            return builder.Build();
+        }
+    }
+}
