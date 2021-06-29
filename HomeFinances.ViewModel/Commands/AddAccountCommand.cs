e962dfe1a69090305c9cadf1ceca99d4a0f1876d
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-29 02:24:12 +0200

    Add AddAccountCommand

---
+++ HomeFinances.ViewModel/Commands/AddAccountCommand.cs
@@ -0,0 +1,30 @@
+﻿using HomeFinances.ViewModel.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace HomeFinances.ViewModel.Commands
+{
+    public class AddAccountCommand : ICommand
+    {
+        private IAddAccountViewModel ViewModel { get; }
+        public event EventHandler CanExecuteChanged;
+
+        public AddAccountCommand(IAddAccountViewModel viewModel)
+        {
+            ViewModel = viewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (String.IsNullOrEmpty(ViewModel.Name) || String.IsNullOrEmpty(ViewModel.Currency) || String.IsNullOrEmpty(ViewModel.Balance)) return false;
+            else return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            ViewModel.AddAccount();
+        }
+    }
+}
