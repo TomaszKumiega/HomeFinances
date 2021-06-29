c9fd626330677cce7972923233307dd17c975a9b
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-29 23:22:58 +0200

    Copy database file on startup if it doesnt exist

--- HomeFinances.XamarinForms/HomeFinances.XamarinForms.Android/MainActivity.cs
+++ HomeFinances.XamarinForms/HomeFinances.XamarinForms.Android/MainActivity.cs
@@ -4,18 +4,22 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using System.IO;
 
 namespace HomeFinances.XamarinForms.Droid
 {
     [Activity(Label = "HomeFinances.XamarinForms", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        private Configuration Configuration { get; set; }
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Configuration = new Configuration();
+            CopyDatabase();
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
@@ -23,6 +27,25 @@ namespace HomeFinances.XamarinForms.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        private void CopyDatabase()
+        {
+            if (!File.Exists(Configuration.DatabaseFilePath))
+            {
+                using (var binaryReader = new BinaryReader(Android.App.Application.Context.Assets.Open(Configuration.DatabaseFileName)))
+                {
+                    using (var binaryWriter = new BinaryWriter(new FileStream(Configuration.DatabaseFilePath, FileMode.Create)))
+                    {
+                        byte[] buffer = new byte[2048];
+                        int length = 0;
+                        while ((length = binaryReader.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            binaryWriter.Write(buffer, 0, length);
+                        }
+                    }
+                }
+            }
         }
     }
 }
