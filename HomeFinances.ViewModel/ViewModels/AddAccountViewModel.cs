2b3bfd8b968426a986478f62256c8bd16564ea65
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-29 02:26:30 +0200

    Add IAddAccountViewModel interface

--- HomeFinances.ViewModel/ViewModels/AddAccountViewModel.cs
+++ HomeFinances.ViewModel/ViewModels/AddAccountViewModel.cs
@@ -1,26 +1,30 @@
 ﻿using HomeFinances.Model.Model;
+using HomeFinances.ViewModel.Commands;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace HomeFinances.ViewModel.ViewModels
 {
-    public class AddAccountViewModel
+    public class AddAccountViewModel : IAddAccountViewModel
     {
         private IDatabaseContext Context { get; }
 
         public string Currency { get; set; }
         public string Name { get; set; }
-        public double Balance { get; set; }
+        public string Balance { get; set; }
+        public ICommand AddAccountCommand { get; }
 
-        public AddAccountViewModel(IDatabaseContext context)
+        public AddAccountViewModel(IDatabaseContext context, ICommandFactory commandFactory)
         {
             Context = context;
+            AddAccountCommand = commandFactory.GetAddAccountCommand(this);
         }
 
         public void AddAccount()
         {
-            var account = new Account(Guid.NewGuid(), Currency, Name, Balance);
+            var account = new Account(Guid.NewGuid(), Currency, Name, double.Parse(Balance));
             Context.Accounts.Add(account);
             Context.SaveChanges();
         }
---
+++ HomeFinances.ViewModel/ViewModels/IAddAccountViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Input;
+
+namespace HomeFinances.ViewModel.ViewModels
+{
+    public interface IAddAccountViewModel
+    {
+        ICommand AddAccountCommand { get; }
+        string Balance { get; set; }
+        string Currency { get; set; }
+        string Name { get; set; }
+
+        void AddAccount();
+    }
+}
