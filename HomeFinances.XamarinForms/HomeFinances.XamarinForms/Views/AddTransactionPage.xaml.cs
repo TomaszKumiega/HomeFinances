5d084ba7e0eeba07a0b462e749d780aa095a1a83
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-30 02:47:15 +0200

    Add AddTransactionPage

---
+++ HomeFinances.XamarinForms/HomeFinances.XamarinForms/Views/AddTransactionPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using HomeFinances.ViewModel.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HomeFinances.XamarinForms.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddTransactionPage : ContentPage
+    {
+        public AddTransactionPage(IAddTransactionViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext = viewModel;
+        }
+    }
+}
