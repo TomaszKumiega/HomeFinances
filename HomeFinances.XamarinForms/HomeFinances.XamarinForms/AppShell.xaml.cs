735b769372cf23266bf6dc11b5497b499bdaf35f
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-07-02 17:33:22 +0200

    Remove unnecessary method

--- HomeFinances.XamarinForms/HomeFinances.XamarinForms/AppShell.xaml.cs
+++ HomeFinances.XamarinForms/HomeFinances.XamarinForms/AppShell.xaml.cs
@@ -12,10 +12,5 @@ namespace HomeFinances.XamarinForms
             InitializeComponent();
             HomeShellContent.Content = homePage;
         }
-
-        private async void OnMenuItemClicked(object sender, EventArgs e)
-        {
-            await Shell.Current.GoToAsync("//HomePage");
-        }
     }
 }
