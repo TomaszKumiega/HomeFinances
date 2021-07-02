c778c2b0f41e90122f6c1a339f19a769ebb04458
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-07-02 17:36:27 +0200

    Add AddCategoryPage

---
+++ HomeFinances.XamarinForms/HomeFinances.XamarinForms/Views/AddCategoryPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using HomeFinances.ViewModel.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HomeFinances.XamarinForms.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddCategoryPage : ContentPage
+    {
+        public AddCategoryPage(IAddCategoryViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext = viewModel;
+        }
+    }
+}
