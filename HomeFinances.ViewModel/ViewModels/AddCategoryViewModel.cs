aeef489b77887b077cfc830d23742e93485a89b3
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-07-02 17:33:09 +0200

    Add AddCategoryViewModel

---
+++ HomeFinances.ViewModel/ViewModels/AddCategoryViewModel.cs
@@ -0,0 +1,85 @@
+﻿using HomeFinances.Model.Model;
+using HomeFinances.ViewModel.Commands;
+using HomeFinances.ViewModel.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Input;
+
+namespace HomeFinances.ViewModel.ViewModels
+{
+    public class AddCategoryViewModel : INotifyPropertyChanged, IAddCategoryViewModel
+    {
+        private IDatabaseContext Context { get; }
+        private DataChangedNotification DataChangedNotification { get; }
+        private string name;
+        private string description;
+        private Color color;
+        private TransactionType transactionType;
+
+        public ICommand AddCategoryCommand { get; set; }
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                RaisePropertyChanged("Name");
+            }
+        }
+
+        public string Description
+        {
+            get => description;
+            set
+            {
+                description = value;
+                RaisePropertyChanged("Description");
+            }
+        }
+
+        public Color Color
+        {
+            get => color;
+            set
+            {
+                color = value;
+                RaisePropertyChanged("Color");
+            }
+        }
+
+        public TransactionType TransactionType
+        {
+            get => transactionType;
+            set
+            {
+                transactionType = value;
+                RaisePropertyChanged("TransactionType");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public AddCategoryViewModel(IDatabaseContext databaseContext, DataChangedNotification dataChangedNotification, ICommandFactory commandFactory)
+        {
+            Context = databaseContext;
+            DataChangedNotification = dataChangedNotification;
+            AddCategoryCommand = commandFactory.GetAddCategoryCommand(this);
+        }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void AddCategory()
+        {
+            var category = new Category(Guid.NewGuid(), Name, Description, Color, TransactionType);
+            Context.Categories.Add(category);
+            Context.SaveChanges();
+            DataChangedNotification.RaiseDataChanged("Categories");
+        }
+    }
+}
---
+++ HomeFinances.ViewModel/ViewModels/IAddCategoryViewModel.cs
@@ -0,0 +1,20 @@
+﻿using HomeFinances.Model.Model;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Input;
+
+namespace HomeFinances.ViewModel.ViewModels
+{
+    public interface IAddCategoryViewModel
+    {
+        ICommand AddCategoryCommand { get; set; }
+        Color Color { get; set; }
+        string Description { get; set; }
+        string Name { get; set; }
+        TransactionType TransactionType { get; set; }
+
+        event PropertyChangedEventHandler PropertyChanged;
+
+        void AddCategory();
+    }
+}
